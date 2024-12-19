74551e1e8ed2bacf7c15dbcf6ba70265bde28e64
JohnW44 <johnwintz@gmail.com>
2024-12-18 16:20:08 -0800

    get song details route complete

--- aa19-python-group-project/app/api/songs_routes.py
+++ aa19-python-group-project/app/api/songs_routes.py
@@ -11,3 +11,14 @@ def songs():
     """
     songs = Song.query.all() 
     return jsonify({'Songs': [song.to_dict() for song in songs]})
+
+@songs_routes.route('/<int:songId>', methods=['GET'])
+def song_details(songId):
+    """
+    Query to return details of a song specified by id
+    """
+    song = Song.query.get(songId)
+    if song is None:
+        return jsonify({"error": "Song couldn't be found"}), 404
+    return jsonify(song.to_dict())
+
