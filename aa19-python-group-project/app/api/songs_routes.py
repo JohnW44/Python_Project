4d73ca2bba92182fd5c8f02bf01db4d8ccb50dd9
JohnW44 <johnwintz@gmail.com>
2024-12-18 22:11:59 -0800

    almost completed update songs route. need to work on image retrieval and update

--- aa19-python-group-project/app/api/songs_routes.py
+++ aa19-python-group-project/app/api/songs_routes.py
@@ -33,12 +33,12 @@ def add_song():
     """
     Adds and Returns a new song when a user is signed in
     """
-    song_data = request.json
+    data = request.json 
 
-    if not song_data.get('Songs'):
+    if not data.get('Songs'):
         return jsonify({ "message": "Bad Request"}), 400
 
-    song_data = ['Songs'][0]
+    song_data = data['Songs'][0]
     song_data['user_id'] = current_user.id
 
     new_song = Song(**song_data)
@@ -47,11 +47,11 @@ def add_song():
 
     response = {'Songs': [new_song.to_dict()]}
 
-    if song_data.get('Images'):
+    if data.get('Images'):
         new_image = Image(
             song_id=new_song.id,
             album_id=song_data['album_id'],
-            url=song_data['Images'][0]['url']
+            url=data['Images'][0]['url']
         )
         db.session.add(new_image)
         response['Images'] = [{
@@ -62,3 +62,33 @@ def add_song():
     return jsonify(response), 201
 
 
+@songs_routes.route('/<int:songId>', methods=['PUT'])
+@login_required
+def update_song():
+    """
+    Updates and returns a song uploaded by user
+    """
+    data = request.json
+
+    song = Song.query.get(songId)
+    if not song:
+        return jsonify({"error": "Song couldn't be found"}), 404
+    if song.user_id != current_user.id:
+        return jsonify({"error": "Unauthorized"}), 403
+    if not data.get('Songs'):
+        return jsonify({"message": "Bad Request"}), 400
+    
+    song_data = data['Songs'][0]
+
+    song.title = song_data['title']
+    song.artist = song_data['artist']
+    song.release_year = song_data['release_year']
+    song.album_id = song_data['album_id']
+    song.lyrics = song_data['lyrics']
+
+    db.session.add(song)
+
+    response = {'Songs': [song.to_dict()]}
+
+    if data.get('Images'):
+        image = Image.query
