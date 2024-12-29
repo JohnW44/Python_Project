b1193c3a98de87ec53414f5564c34086ac288576
JohnW44 <johnwintz@gmail.com>
2024-12-29 13:15:16 -0800

    added my old code and troubleshooting logining in again

--- aa19-python-group-project/frontend/react-vite/src/components/Navigation/Navigation.jsx
+++ aa19-python-group-project/frontend/react-vite/src/components/Navigation/Navigation.jsx
@@ -1,14 +1,20 @@
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
+import { useSelector } from "react-redux";
 import "./Navigation.css";
 
 function Navigation() {
+  const sessionUser = useSelector(state => state.session.user);
   return (
     <ul>
       <li>
         <NavLink to="/">Home</NavLink>
       </li>
-
+      {sessionUser && (
+        <li>
+          <NavLink to="/songs/new">Add Song</NavLink>
+        </li>
+      )}
       <li>
         <ProfileButton />
       </li>
