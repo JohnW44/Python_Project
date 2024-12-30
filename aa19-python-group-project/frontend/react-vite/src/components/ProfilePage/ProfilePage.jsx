97729c0c2fa3eaca6d61a70563ae5db5d60a1444
Dan4dan4 <danm44@hotmail.com>
2024-12-30 16:29:39 -0500

    added profile page take1

--- aa19-python-group-project/frontend/react-vite/src/components/ProfilePage/ProfilePage.jsx
+++ aa19-python-group-project/frontend/react-vite/src/components/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,35 @@
+import { useEffect } from 'react'
 import './ProfilePage.css'
+import { thunkAuthenticate } from '../../redux/session';
+import { useDispatch, useSelector} from 'react-redux';
 
 function ProfilePage(){
 
+const dispatch = useDispatch();
+const user = useSelector(state => state.session.user)
+
+useEffect(() => {
+    dispatch(thunkAuthenticate())
+}, [dispatch])
+
+
+
+    return (
+        <>
+            <div className='profilebox'>
+                <h1>Profile Page</h1>
+                <div className='profileinfo'>
+                    <img src={user.profile_image} className='profile-pic'/>
+                    <div className='details'>
+                        <p>Username: {user.username}</p>
+                        <p>Email: {user.email}</p>
+                        <p>First Name: {user.first_name}</p>
+                        <p>Last Name: {user.last_name}</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 
