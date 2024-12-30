eb0026013943e8064e0eb07a77fbf7197f67bc26
Dan4dan4 <danm44@hotmail.com>
2024-12-30 14:17:03 -0500

    landing page is getting details from backend correctly

--- aa19-python-group-project/frontend/react-vite/src/components/LandingPage/LandingPage.jsx
+++ aa19-python-group-project/frontend/react-vite/src/components/LandingPage/LandingPage.jsx
@@ -1,40 +1,45 @@
-// import { useNavigate } from 'react-router-dom';
-// import { useDispatch } from 'react-redux';
 import './LandingPage.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+
 function LandingPage() {
-//    const navigate = useNavigate();
-//    const dispatch = useDispatch();
-    // Placeholder data
-//    const [albums, setAlbums] = useState([]);
-const albums = [
-    { id: 1, title: 'Album 1', imageUrl: 'url1' },
-    { id: 2, title: 'Album 2', imageUrl: 'url2' }]
+
+const [albums, setAlbums] = useState([]);
 
    useEffect(() => {
     const fetchalbum = async () => {
-        const response = await fetch ('/albums');
-        const data = await response.json();
-        albums(data)
-    };
-        fetchalbum()
-   })
+        const response = await fetch('http://localhost:5000/albums'); 
+
+            if (response.ok) {
+                const data = await response.json(); 
+                console.log('Fetched albums:', data);
+                if (data.Albums) {
+                    setAlbums(data.Albums);
+                }
+            }
+        };
+
+        fetchalbum();
+    }, []);
 
     return (
        <div className="landing-page-container">
            <div className="playlist-sidebar">
-               {/* Playlist section */}
                <h2>Playlists</h2>
                <ul className="playlist-list">
-                   {/* Add your playlists here */}
                </ul>
            </div>
             <div className="main-content">
                <div className="albums-grid">
                    {albums.map(album => (
-                       <div key={album.artist} className="album-circle">
-                           <img src={albums.images} alt={albums.title} />
+                       <div key={album.id} className="album-circle">
+                       {album.images && album.images.length > 0 && album.images[0].url ? (
+                           <img src={album.images[0].url} />
+                       ) : (
+                           <div>No image available</div> 
+                       )}
                            <p>{album.title}</p>
+                           <p>{album.artist}</p>
                        </div>
                    ))}
                </div>
