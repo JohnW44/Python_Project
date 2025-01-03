892920cdd9159d402a07ab0e1f698116f5c2857e
Dan4dan4 <danm44@hotmail.com>
2025-01-03 18:09:06 -0500

    changed dockerfile to have init/migrate and dockignore to ignore migrations

--- migrations/versions/6b054c905e8f_.py
+++ migrations/versions/6b054c905e8f_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 0e7b41887dc7
+Revision ID: 6b054c905e8f
 Revises: 
-Create Date: 2025-01-03 16:53:31.975899
+Create Date: 2025-01-03 17:52:38.381412
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0e7b41887dc7'
+revision = '6b054c905e8f'
 down_revision = None
 branch_labels = None
 depends_on = None
