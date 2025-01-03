f856e21fa83d1b077576d3e8cf8ed39b2281aa30
Dan4dan4 <danm44@hotmail.com>
2025-01-03 18:41:04 -0500

    fixed seed typo 2

--- app/seeds/albums.py
+++ app/seeds/albums.py
@@ -35,11 +35,11 @@ def seed_albums():
     db.session.commit()
 
 
-    image1 = Image(
-        url="https://cdn-p.smehost.net/sites/35faef12c1b64b21b3fda052d205af13/wp-content/uploads/2023/02/230222-daftpunk-ram10.jpg",
-        album_id=album1.id,
-        song_id=1 
-    )
+    # image1 = Image(
+    #     url="https://cdn-p.smehost.net/sites/35faef12c1b64b21b3fda052d205af13/wp-content/uploads/2023/02/230222-daftpunk-ram10.jpg",
+    #     album_id=album1.id,
+    #     song_id=1 
+    # )
     
     # image2 = Image(
     #     url="https://cdn-p.smehost.net/sites/35faef12c1b64b21b3fda052d205af13/wp-content/uploads/2023/02/230222-daftpunk-ram10.jpg",
@@ -47,9 +47,9 @@ def seed_albums():
     #     song_id=2
     # )
 
-    db.session.add(image1)
+    # db.session.add(image1)
     # db.session.add(image2)
-    db.session.commit()
+    # db.session.commit()
     
 def undo_albums():
     if environment == "production":
